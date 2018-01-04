e94da923355e4c652f11fe43e5b4a65df8eb4576
Artorias <knight821714509@users.noreply.github.com>
2018-01-04 21:24:12 +0800

    send date

--- app/Http/Controllers/Article/FindArticleController.php
+++ app/Http/Controllers/Article/FindArticleController.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\Controller;
 
 class FindArticleController extends Controller{
     public function findArticle(){
-        return view('test')->with('Article',['title'=>'标题','auth'=>'管理员','content'=>'文章内容']);
+        return view('test')->with('Article',['title'=>'标题','auth'=>'管理员','content'=>'文章内容','date'=>'2018']);
     }
 }
