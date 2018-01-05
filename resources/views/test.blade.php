bff7b6ef996294953205ab1d7efa40a467be1ac5
Artorias <Knight821714509@users.noreply.github.com>
2018-01-05 10:46:43 +0800

    url
    css&js file import

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <title>test</title>
-    <link rel="stylesheet" href="../../public/css/bootstrap.css">
-    <script src="../../public/js/jQuery.js"></script>
-    <script src="../../public/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="{{URL::asset('css/bootstrap.css')}}">
+    <script src="{{URL::asset('js/jQuery.js')}}"></script>
+    <script src="{{URL::asset('js/bootstrap.js')}}"></script>
 </head>
 <body>
 <div class="container">
