dd38c5e5c6dd1209f28b7d647a722eff176382bb
Artorias <Knight821714509@users.noreply.github.com>
2018-01-11 14:41:28 +0800

    内容页

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -9,8 +9,8 @@
     <div class="col-md-12">
         <h2 class="text-center">{{$article->title}}</h2>
         <br>
-        <h5 class="text-center"><?php echo $auth .'   点击：'.$article->view.'次   发布时间:'.date('Y/m/d',strtotime($article->create_time));?> </h5>
+        <h5 class="text-center">{{$auth .'   点击：'.$article->view.'次   发布时间:'.date('Y/m/d',strtotime($article->create_time))}} </h5>
         <hr>
-        <?php echo $article->content ?>
+        {!! $article->content !!}
     </div>
 @endsection
