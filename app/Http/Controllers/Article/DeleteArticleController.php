0657b714cc0df3f63286d8173b67f0368d2465b5
Artorias <Knight821714509@users.noreply.github.com>
2018-03-15 19:04:29 +0800

    删除文章

--- app/Http/Controllers/Article/DeleteArticleController.php
+++ app/Http/Controllers/Article/DeleteArticleController.php
@@ -14,6 +14,6 @@ class DeleteArticleController extends Controller{
     public function deleteArticleById($id){
         Article::where('id',$id)
             ->delete();
-        echo '<script>alert("删除成功");history.go(-1);</script>';
+        echo '<script>alert("删除成功");window.location.href="'.url('sel-art').'";</script>';
     }
 }
---
+++ app/Http/Controllers/Article/UpdateArticleController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artorias
+ * Date: 2018/3/15
+ * Time: 18:35
+ */
+namespace App\Http\Controllers\Article;
+
+use App\Http\Controllers\Controller;
+
+class UpdateArticleController extends Controller{
+    public function updateArticleById($id){
+        
+    }
+}
